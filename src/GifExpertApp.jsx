4bc19e5d9dbc99cbdbfcc647633dccba99bf052c
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-09-13 17:30:29 -0500

    Adding new Category 'Valorant'

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = () => {
+    setCategories(["Valorant", ...categories]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <button onClick={onAddCategory}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
