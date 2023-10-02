b227011919c0b4e92139d67e2b6c97d979b8916c
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2023-10-01 21:40:40 -0500

    Using custom hook to fetching gifs

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -1,24 +1,15 @@
 import PropTypes from "prop-types";
-import { getGifs } from "../helpers/getGifs";
-import { useEffect, useState } from "react";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+
+      {isLoading && <h2>Cargando...</h2>}
 
       <div className="card-grid">
         {images.map((image) => (
